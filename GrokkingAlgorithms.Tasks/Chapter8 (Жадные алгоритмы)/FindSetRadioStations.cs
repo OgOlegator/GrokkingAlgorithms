d28a11b07ea208342daba8640760878dff924b1e
OgOlegator <92753056+OgOlegator@users.noreply.github.com>
2024-09-10 00:14:04 +0300

    Add tasks from chapter 8

---
+++ GrokkingAlgorithms.Tasks/Chapter8 (Жадные алгоритмы)/FindSetRadioStations.cs
@@ -0,0 +1,50 @@
+﻿namespace GrokkingAlgorithms.Tasks.Chapter8__Жадные_алгоритмы_
+{
+    /// <summary>
+    /// Найти наименьший набор радиостанций, покрывающих заданный набор штатов
+    /// </summary>
+    public class FindSetRadioStations
+    {
+        /// <summary>
+        /// Радиостанции и покрываемые ими штаты
+        /// </summary>
+        private readonly Dictionary<string, string[]> _radioStations;
+
+        public FindSetRadioStations(Dictionary<string, string[]> radioStations)
+        {
+            _radioStations = radioStations;
+        }
+
+        public List<string> Execute(IEnumerable<string> statesNeeded)
+        {
+            var result = new List<string>();
+
+            while (statesNeeded.Count() > 0)
+            {
+                // Наибольшее покрытие штатов радиостанцией
+                var statesCovered = new List<string>();
+
+                // Название радиостанции с наибольшим покрытием
+                var bestStationName = string.Empty;
+
+                foreach (var station in _radioStations)
+                {
+                    var covered = station.Value.Intersect(statesNeeded).ToList();
+
+                    if (covered.Count() > statesCovered.Count())
+                    {
+                        bestStationName = station.Key;
+                        statesCovered = covered;
+                    }
+                }
+
+                statesNeeded = statesNeeded.Except(statesCovered);
+                result.Add(bestStationName);
+            }
+
+            return result;
+        }
+
+
+    }
+}
---
+++ GrokkingAlgorithms.Tests/Chapter8/FindSetRadioStationsTests.cs
@@ -0,0 +1,29 @@
+﻿using GrokkingAlgorithms.Tasks.Chapter8__Жадные_алгоритмы_;
+
+namespace GrokkingAlgorithms.Tests.Chapter8
+{
+    public class FindSetRadioStationsTests
+    {
+        [Theory]
+        [InlineData(new string[] { "mt", "wa", "or", "id", "nv", "ut", "ca", "az" }, new string[] { "kone", "ktwo", "kthree", "kfive" })]
+        [InlineData(new string[] { "fi", "fl", "tx", "id", "nv" }, new string[] { "ksix" })]
+        [InlineData(new string[] { "fi", "fl", "tx", "id", "ut" }, new string[] { "ksix", "kone" })]
+        public void Execute(string[] inArr, string[] outArr)
+        {
+            Assert.Equal(outArr.ToList(), new FindSetRadioStations(GetTestData()).Execute(inArr.ToList()));
+        }
+
+        private Dictionary<string, string[]> GetTestData()
+        {
+            return new Dictionary<string, string[]>
+            {
+                { "kone", new string[] { "id", "nv", "ut" } },
+                { "ktwo", new string[] { "wa", "id", "mt" } },
+                { "kthree", new string[] { "or", "nv", "ca" } },
+                { "kfour", new string[] { "nv", "ut" } },
+                { "kfive", new string[] { "ca", "az" } },
+                { "ksix", new string[] { "fi", "fl", "tx", "id", "nv" } }
+            };
+        }
+    }
+}
